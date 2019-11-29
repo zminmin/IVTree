796d8fa9c77822a1cfca3ed8a4910388429e3ffc
gweehwa <gweehwa@gmail.com>
2019-11-29 10:04:58 -0600

    Update CT.c

--- src/CT.c
+++ src/CT.c
@@ -37,7 +37,7 @@ int CTinit(int n, double *y[], int maxcat, char **error,
 }
 
 
-
+%%
 void CTss(int n, double *y[], double *value, double *con_mean, double *tr_mean, 
      double *risk, double *wt, double *treatment, double *treatment1, double *IV, double max_y,
      double alpha, double train_to_est_ratio) {
